478abed109bf57a7023798002b8922e87987aff0
Venky-369 <venkateshdorolla369@gmail.com>
2024-05-21 22:17:30 -0500

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,22 +4,21 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.cv 
-
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+// Configure the HTTP request pipeline.
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
